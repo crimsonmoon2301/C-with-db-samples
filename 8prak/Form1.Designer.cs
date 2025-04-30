75d04c5f5c9ab47c12b9c27a0f520a7525277dff
A Something that is nothing <150836006+crimsonmoon2301@users.noreply.github.com>
2025-04-30 17:04:27 +0300

    Add files via upload

---
+++ 8prak/Form1.Designer.cs
@@ -0,0 +1,132 @@
+﻿namespace _8.prak
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.dataGridViewStudents = new System.Windows.Forms.DataGridView();
+            this.dataGridViewProgrammas = new System.Windows.Forms.DataGridView();
+            this.dataGridViewFakultates = new System.Windows.Forms.DataGridView();
+            this.btnRefresh = new System.Windows.Forms.ToolStripButton();
+            this.btnSave = new System.Windows.Forms.ToolStripButton();
+            this.toolStrip1 = new System.Windows.Forms.ToolStrip();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridViewStudents)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridViewProgrammas)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridViewFakultates)).BeginInit();
+            this.toolStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // dataGridViewStudents
+            // 
+            this.dataGridViewStudents.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridViewStudents.Location = new System.Drawing.Point(12, 241);
+            this.dataGridViewStudents.Name = "dataGridViewStudents";
+            this.dataGridViewStudents.Size = new System.Drawing.Size(646, 187);
+            this.dataGridViewStudents.TabIndex = 7;
+            // 
+            // dataGridViewProgrammas
+            // 
+            this.dataGridViewProgrammas.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridViewProgrammas.Location = new System.Drawing.Point(363, 51);
+            this.dataGridViewProgrammas.Name = "dataGridViewProgrammas";
+            this.dataGridViewProgrammas.Size = new System.Drawing.Size(345, 184);
+            this.dataGridViewProgrammas.TabIndex = 6;
+            // 
+            // dataGridViewFakultates
+            // 
+            this.dataGridViewFakultates.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridViewFakultates.Location = new System.Drawing.Point(12, 51);
+            this.dataGridViewFakultates.Name = "dataGridViewFakultates";
+            this.dataGridViewFakultates.Size = new System.Drawing.Size(345, 184);
+            this.dataGridViewFakultates.TabIndex = 5;
+            // 
+            // btnRefresh
+            // 
+            this.btnRefresh.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Text;
+            this.btnRefresh.Image = ((System.Drawing.Image)(resources.GetObject("btnRefresh.Image")));
+            this.btnRefresh.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.btnRefresh.Name = "btnRefresh";
+            this.btnRefresh.Size = new System.Drawing.Size(75, 22);
+            this.btnRefresh.Text = "Atsvaidzināt";
+            this.btnRefresh.Click += new System.EventHandler(this.btnRefresh_Click_1);
+            // 
+            // btnSave
+            // 
+            this.btnSave.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Text;
+            this.btnSave.Image = ((System.Drawing.Image)(resources.GetObject("btnSave.Image")));
+            this.btnSave.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.btnSave.Name = "btnSave";
+            this.btnSave.Size = new System.Drawing.Size(56, 22);
+            this.btnSave.Text = "Saglabāt";
+            this.btnSave.Click += new System.EventHandler(this.btnSave_Click_1);
+            // 
+            // toolStrip1
+            // 
+            this.toolStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.btnRefresh,
+            this.btnSave});
+            this.toolStrip1.Location = new System.Drawing.Point(0, 0);
+            this.toolStrip1.Name = "toolStrip1";
+            this.toolStrip1.Size = new System.Drawing.Size(800, 25);
+            this.toolStrip1.TabIndex = 8;
+            this.toolStrip1.Text = "toolStrip1";
+            this.toolStrip1.ItemClicked += new System.Windows.Forms.ToolStripItemClickedEventHandler(this.toolStrip1_ItemClicked);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.dataGridViewStudents);
+            this.Controls.Add(this.dataGridViewProgrammas);
+            this.Controls.Add(this.dataGridViewFakultates);
+            this.Controls.Add(this.toolStrip1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridViewStudents)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridViewProgrammas)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridViewFakultates)).EndInit();
+            this.toolStrip1.ResumeLayout(false);
+            this.toolStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGridViewStudents;
+        private System.Windows.Forms.DataGridView dataGridViewProgrammas;
+        private System.Windows.Forms.DataGridView dataGridViewFakultates;
+        private System.Windows.Forms.ToolStripButton btnRefresh;
+        private System.Windows.Forms.ToolStripButton btnSave;
+        private System.Windows.Forms.ToolStrip toolStrip1;
+    }
+}
+
---
+++ 8prak/Form1.cs
@@ -0,0 +1,210 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Npgsql;
+
+namespace _8.prak
+{
+    public partial class Form1: Form
+    {
+        private DataSet ds = new DataSet();
+        private NpgsqlConnection conn;
+        private NpgsqlDataAdapter daStudents, daProgramma, daFakultate;
+
+        public Form1()
+        {
+            InitializeComponent();
+            dataGridViewStudents.CellClick += dataGridViewStudents_CellClick;
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            string connStr = "Host=localhost;Port=5432;Username=postgres;Password=students;Database=Student";
+            conn = new NpgsqlConnection(connStr);
+            
+            try
+            {
+                daFakultate = new NpgsqlDataAdapter("SELECT * FROM Fakultate", conn);
+                daProgramma = new NpgsqlDataAdapter("SELECT * FROM Programma", conn);
+                daStudents = new NpgsqlDataAdapter("SELECT * FROM Students", conn);
+
+                // Šīs 3 rindas ir ļoti svarīgas
+                daFakultate.MissingSchemaAction = MissingSchemaAction.AddWithKey;
+                daProgramma.MissingSchemaAction = MissingSchemaAction.AddWithKey;
+                daStudents.MissingSchemaAction = MissingSchemaAction.AddWithKey;
+
+                // Tagad CommandBuilder var uzģenerēt komandas korekti
+                new NpgsqlCommandBuilder(daFakultate);
+                new NpgsqlCommandBuilder(daProgramma);
+                new NpgsqlCommandBuilder(daStudents);
+
+                daFakultate.Fill(ds, "Fakultate");
+                daProgramma.Fill(ds, "Programma");
+                daStudents.Fill(ds, "Students");
+
+                daFakultate.AcceptChangesDuringUpdate = true;
+                daStudents.AcceptChangesDuringUpdate = true;
+                daProgramma.AcceptChangesDuringUpdate = true ;
+
+                ds.Tables["Fakultate"].TableName = "Fakultate";
+                ds.Tables["Programma"].TableName = "Programma";
+                ds.Tables["Students"].TableName = "Students";
+
+                ds.Relations.Add(new DataRelation("FK_Programma_Fakultate",
+                    ds.Tables["Fakultate"].Columns["Id_F"],
+                    ds.Tables["Programma"].Columns["Id_F"], true));
+
+                ds.Relations.Add(new DataRelation("FK_Students_Programma",
+                    ds.Tables["Programma"].Columns["Id_SP"],
+                    ds.Tables["Students"].Columns["Id_SP"], true));
+
+                dataGridViewFakultates.DataSource = ds.Tables["Fakultate"];
+                dataGridViewProgrammas.DataSource = ds.Tables["Programma"];
+                dataGridViewStudents.DataSource = ds.Tables["Students"];
+
+                dataGridViewStudents.Columns["Id"].Visible = false;
+
+
+
+                DataGridViewComboBoxColumn cbSP = new DataGridViewComboBoxColumn
+                {
+                    HeaderText = "Studiju programma",
+                    DataSource = ds.Tables["Programma"],
+                    DisplayMember = "Nosaukums",
+                    ValueMember = "Id_SP",
+                    DataPropertyName = "Id_SP"
+                };
+                dataGridViewStudents.Columns.Add(cbSP);
+                dataGridViewStudents.Columns["Id_SP"].Visible = false;
+
+                DataGridViewComboBoxColumn cbF = new DataGridViewComboBoxColumn
+                {
+                    HeaderText = "Fakultāte",
+                    DataSource = ds.Tables["Fakultate"],
+                    DisplayMember = "Nosaukums",
+                    ValueMember = "Id_F",
+                    DataPropertyName = "Id_F"
+                };
+
+                DataGridViewButtonColumn btnDetails = new DataGridViewButtonColumn
+                {
+                    HeaderText = "Darbība",
+                    Text = "Visi dati",
+                    UseColumnTextForButtonValue = true,
+                    Name = "btnVisiDati"
+                };
+                dataGridViewStudents.Columns.Insert(0, btnDetails);
+
+                dataGridViewProgrammas.Columns.Add(cbF);
+                dataGridViewProgrammas.Columns["Id_F"].Visible = false;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Kļūda pievienojot datu avotus: " + ex.Message);
+            }
+        }
+
+        // Messagebox
+        private void dataGridViewStudents_CellClick(object sender, DataGridViewCellEventArgs e)
+        {
+            if (e.RowIndex >= 0 && dataGridViewStudents.Columns[e.ColumnIndex].Name == "btnVisiDati")
+            {
+                DataRowView rowView = (DataRowView)dataGridViewStudents.Rows[e.RowIndex].DataBoundItem;
+                DataRow row = rowView.Row;
+
+                DataRow programmaRow = row.GetParentRow("FK_Students_Programma");
+                string programName = "Nezināma";
+                string fakultateName = "Nezināma";
+                string birthYear = "Nezināms";
+                string gender = "Nezināms";
+
+                if (programmaRow != null)
+                {
+                    programName = programmaRow["Nosaukums"].ToString();
+
+                    DataRow fakultateRow = programmaRow.GetParentRow("FK_Programma_Fakultate");
+                    if (fakultateRow != null)
+                    {
+                        fakultateName = fakultateRow["Nosaukums"].ToString();
+                    }
+                }
+
+                // Get the birth year and gender from the current row
+                birthYear = row["dzimsanas_gads"].ToString();
+                gender = row["dzimums"].ToString();
+
+                // Display all the information in the MessageBox
+                string studentInfo = $"ID: {row["Id"]}\n" +
+                                     $"Vārds: {row["Vards"]}\n" +
+                                     $"Uzvārds: {row["Uzvards"]}\n" +
+                                     $"Studiju programma: {programName}\n" +
+                                     $"Fakultāte: {fakultateName}\n" +
+                                     $"Dzimšanas gads: {birthYear}\n" +
+                                     $"Dzimums: {gender}";
+
+                MessageBox.Show(studentInfo, "Studentu dati", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+        }
+
+        private void btnRefresh_Click_1(object sender, EventArgs e)
+        {
+            try
+            {
+                RefreshData();
+                MessageBox.Show("Dati veiksmīgi atsvaidzināti!", "Atsvaidzināts", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Kļūda atsvaidzinot datus: " + ex.Message);
+            }
+        }
+
+        private void toolStrip1_ItemClicked(object sender, ToolStripItemClickedEventArgs e)
+        {
+
+        }
+
+        private void btnSave_Click_1(object sender, EventArgs e)
+        {
+            try
+            {
+                daFakultate.Update(ds.Tables["Fakultate"]);
+                daProgramma.Update(ds.Tables["Programma"]);
+                daStudents.Update(ds.Tables["Students"]);
+
+                MessageBox.Show("Izmaiņas saglabātas veiksmīgi!", "Saglabāts", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Kļūda saglabājot: " + ex.Message);
+            }
+        }
+
+        private void RefreshData()
+        {
+            try
+            {
+                ds.Clear();
+
+                daFakultate.Fill(ds, "Fakultate");
+                daProgramma.Fill(ds, "Programma");
+                daStudents.Fill(ds, "Students");
+
+                dataGridViewFakultates.DataSource = ds.Tables["Fakultate"];
+                dataGridViewProgrammas.DataSource = ds.Tables["Programma"];
+                dataGridViewStudents.DataSource = ds.Tables["Students"];
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Kļūda atsvaidzinot datus no datu bāzes: " + ex.Message);
+            }
+        }
+    }
+}
